dcf7e7e3f13ce27557c35dcbc39bd3279f83874e
Tiago Ferezin <tiagoferezin@gmail.com>
2016-04-02 19:07:36 -0300

    Criacao do Usuario e UsuarioController

---
+++ web/src/main/java/br/com/tfdesenvolve/chromac/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package br.com.tfdesenvolve.chromac.controller;
+
+/**
+ * @author Tiago Ferezin ( Data: 02/04/2016 );
+ * Funcionalidade da Classe:
+ *
+ */
+public class UsuarioController {
+
+}
---
+++ web/src/main/java/br/com/tfdesenvolve/chromac/model/acesso/Usuario.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package br.com.tfdesenvolve.chromac.model.acesso;
+
+/**
+ * @author Tiago Ferezin ( Data: 02/04/2016 );
+ * Funcionalidade da Classe:
+ *
+ */
+public class Usuario {
+
+}
